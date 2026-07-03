a6a80260dbd886ff4919dd4409e45a9d71312a98
raenaldiardiansyah <raenaldi.ardiansyah30@gmail.com>
2026-07-03 22:20:47 +0800

    chore: remove turbopack root to fix vercel warning

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: process.cwd(),
-  }
+  // Tambahkan konfigurasi Next.js lainnya di sini jika diperlukan
 };
 
 export default nextConfig;
